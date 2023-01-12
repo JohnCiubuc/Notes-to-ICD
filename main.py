02098226d60476d79e7f4ebbcf48ca100e2bd328
John Ciubuc <john.ciubuc@ttuhsc.edu>
2023-01-12 16:59:41 -0600

    Debug cache for aws

--- main.py
+++ main.py
@@ -7,6 +7,15 @@ Created on Mon Jan 9 12:18:26 2023
 """
 import aws
 import re
+import pickle
+
+#  ===============================
+#  ========== DEBUG ==============
+#  ===============================
+DEBUG_USE_PICKLE = True
+#  ===============================
+#  ===============================
+#  ===============================
 
 
 f = open('DID_Notes/DID1.txt')
@@ -15,17 +24,42 @@ note = f.read()
 # Reformat note to only send the sections relevant to be NLP'd
 # specifically, HPI and A&P
 # Note format must be known in advanced.
+# Sections added in list rather than appending for processing later
 clip_sections = [('Reason For Visit', 'Review of Systems'), ('Assessment', 'END')]
-
 note_sections = []
 for section in clip_sections:
     start = note.find(section[0])
     end = note.find(section[1]) if section[1] != 'END' else -1
-    note_sections.append(note[start:end])    
+    note_sections.append(note[start:end])   
+    
 
-# response = comp.detect_entities(Text='Encounter for wellness examination in adult. Patient has a complex renal cyst')
-# response = aws.detectEntities(note)
 
-# a = response['Entities']
+# Recombine sections for aws
+# Count each section size for processing
+note = ''
+note_section_indexes = []
+for section in note_sections:
+    note = note + section + '\n'
+    if len(note_section_indexes) > 0:
+        note_section_indexes.append(note_section_indexes[-1] + len(section))
+    else: 
+        note_section_indexes.append(len(section))
+
+
+# Request entities
+if DEBUG_USE_PICKLE:
+    file = open('aws_response.pkl', 'rb')
+    response = pickle.load(file)
+    entities  = response['Entities']
+    file.close()
+else:
+    response = aws.detectEntities(note)
+    entities  = response['Entities']
+
+# save to not abuse api while testing
+file = open('aws_response.pkl', 'wb')
+pickle.dump(response, file)
+file.close()
+
 # for entity in a:
 #     print('Entity', entity)
