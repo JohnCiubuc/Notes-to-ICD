49559b076500bd2db58fd9a3991522c1a77e7abe
John Ciubuc <john.ciubuc@ttuhsc.edu>
2023-01-12 17:37:55 -0600

    icd10 db

--- snomed.py
+++ snomed.py
@@ -5,11 +5,18 @@ Created on Thu Jan 12 17:12:09 2023
 
 @author: inathero
 """
-
 # from pymedtermino import *
 from pymedtermino.snomedct import SNOMEDCT
+from pymedtermino.icd10 import ICD10
+SNOMEDCT_DIR = "/home/jiba/telechargements/base_med/SnomedCT_Release_INT_20130731"
+SNOMEDCT_CORE_FILE = "/home/jiba/telechargements/base_med/SNOMEDCT_CORE_SUBSET_201308.txt"
+ICD10_DIR = "/home/jiba/telechargements/base_med/icd10"
+CIM10_DIR = "/home/jiba/telechargements/base_med/cim10"
 
 # a=SNOMEDCT.search('insomnia')
 
 def search(text):
     return SNOMEDCT.search(text)
+
+def getICD(snomedCode):
+    return ICD10[snomedCode]
