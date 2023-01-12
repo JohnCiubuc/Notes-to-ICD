7af59bc23f685cb2c4b81d0459fd75b5636da515
John Ciubuc <john.ciubuc@ttuhsc.edu>
2023-01-12 16:44:31 -0600

    Reformat aws out of main

---
+++ aws.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 12 16:42:36 2023
+
+@author: John Ciubuc
+"""
+import boto3
+import os
+import configparser
+
+from pymedtermino import *
+from pymedtermino.snomedct import SNOMEDCT
+
+# a=SNOMEDCT.search('insomnia')
+
+config = configparser.ConfigParser()
+config.read(os.path.expanduser("~") + '/.aws/credentials')
+
+boto3.Session(aws_access_key_id=config.get('default','aws_access_key_id') ,                     
+              aws_secret_access_key=config.get('default','aws_secret_access_key') ,
+              region_name='us-east-2')
+
+comp = boto3.client('comprehendmedical')
+
+
+def detectEntities(text):
+    return comp.detect_entities(Text=text)
--- main.py
+++ main.py
@@ -5,31 +5,25 @@ Created on Mon Jan 9 12:18:26 2023
 
 @author: John Ciubuc
 """
-import boto3
-import os
-import configparser
-
-
-from pymedtermino import *
-from pymedtermino.snomedct import SNOMEDCT
-
-
+import aws
 
 # a=SNOMEDCT.search('insomnia')
 
-config = configparser.ConfigParser()
-config.read(os.path.expanduser("~") + '/.aws/credentials')
-
-boto3.Session(aws_access_key_id=config.get('default','aws_access_key_id') ,                     
-              aws_secret_access_key=config.get('default','aws_secret_access_key') ,
-              region_name='us-east-2')
-
-comp = boto3.client('comprehendmedical')
 f = open('DID_Notes/DID1.txt')
-did = f.read()
+note = f.read()
+
+f = open('DID_Notes/DID1.txt')
+note = f.read()
+
+# Reformat note to only send the sections relevant to be NLP'd
+# specifically, HPI and A&P
+# Note format must be known in advanced.
+
+clip_sections = [('Reason For Visit', 'Review of Systems'), ('Assessment', 'END')]
+
 
 # response = comp.detect_entities(Text='Encounter for wellness examination in adult. Patient has a complex renal cyst')
-response = comp.detect_entities(Text=did)
+response = aws.detectEntities(note)
 
 a = response['Entities']
 for entity in a:
