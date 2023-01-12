feba26f9c685fe6075abffe31c3db7169b84ad0c
John Ciubuc <john.ciubuc@ttuhsc.edu>
2023-01-12 16:37:26 -0600

    Uploads one note and gets entities

--- main.py
+++ main.py
@@ -9,6 +9,14 @@ import boto3
 import os
 import configparser
 
+
+from pymedtermino import *
+from pymedtermino.snomedct import SNOMEDCT
+
+
+
+# a=SNOMEDCT.search('insomnia')
+
 config = configparser.ConfigParser()
 config.read(os.path.expanduser("~") + '/.aws/credentials')
 
@@ -17,7 +25,12 @@ boto3.Session(aws_access_key_id=config.get('default','aws_access_key_id') ,
               region_name='us-east-2')
 
 comp = boto3.client('comprehendmedical')
-response = comp.detect_entities(Text='Encounter for wellness examination in adult. Patient has a complex renal cyst')
+f = open('DID_Notes/DID1.txt')
+did = f.read()
+
+# response = comp.detect_entities(Text='Encounter for wellness examination in adult. Patient has a complex renal cyst')
+response = comp.detect_entities(Text=did)
+
 a = response['Entities']
 for entity in a:
     print('Entity', entity)
